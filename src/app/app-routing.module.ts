139a4aaaf7dddf909680e648353d814d815f85bb
Regiane Eugenio <regiane21.eugenio92@gmail.com>
2019-05-28 18:40:29 -0300

    28/05/2019

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './usuario/login/login.module#LoginPageModule' },
+  { path: 'perfil', loadChildren: './usuario/perfil/perfil.module#PerfilPageModule' },
+  { path: 'add', loadChildren: './produto/add/add.module#AddPageModule' },
+  { path: 'detail/:id', loadChildren: './produto/detail/detail.module#DetailPageModule' },
+  { path: 'car', loadChildren: './produto/car/car.module#CarPageModule' },
+  { path: 'map', loadChildren: './usuario/map/map.module#MapPageModule' },  { path: 'login', loadChildren: './usuario/login/login.module#LoginPageModule' }
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
